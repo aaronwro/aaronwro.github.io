ea6f963576d823abaa280cc72881a2e2d808bb25
Aaron Wroblewski <aaronwro@gmail.com>
2020-08-09 14:37:36 -0700

    Updates

---
+++ precache-manifest.d93a6afbf7de5db0e420e6c91f328ace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9156fd8aeb2dd860b6f830ed1c943f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd6a4c2d5157d0b05449",
+    "url": "/static/css/main.b2d4de48.chunk.css"
+  },
+  {
+    "revision": "27db906bc81ec7da4de3",
+    "url": "/static/js/2.1ec008dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ec008dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6a4c2d5157d0b05449",
+    "url": "/static/js/main.d55d49c9.chunk.js"
+  },
+  {
+    "revision": "b20214cd473c1eb3b22f",
+    "url": "/static/js/runtime-main.f3efb0cf.js"
+  }
+]);
